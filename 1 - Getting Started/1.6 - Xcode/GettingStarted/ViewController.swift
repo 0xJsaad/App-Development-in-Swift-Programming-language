d2deb8b6ad63db8c09d7f56367b16e863ab015c2
0xJs <jpsaad00@gmail.com>
2023-10-31 19:10:45 -0400

    Section 1.6 - Xcode Completed [ x ]

---
+++ 1 - Getting Started/1.6 - Xcode/GettingStarted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Juan Saad on 10/31/2023.
+//  Copyright © 2023 Juan Saad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
